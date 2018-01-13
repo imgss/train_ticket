ab3ebb5a0b8e2db1ef74eb68dfa5814223c832f5
imgss <838632526@qq.com>
2018-01-13 23:35:28 +0800

    优化打印

--- index.py
+++ index.py
@@ -42,23 +42,26 @@ date = '2018-02-11'
 stations = read_station()# 车站和车站代码的转换
 sites = {"软卧":23,"无座":26,"硬卧":28,"硬座":29,"二等":30,"一等":31,"商务":32}# 数据的索引
 print('%s => %s' %(stations[start], stations[to]))
+counter = 0
 while True:
     with request.urlopen('https://kyfw.12306.cn/otn/leftTicket/queryZ?leftTicketDTO.train_date=' + date + '&leftTicketDTO.from_station='+ stations[start]+'&leftTicketDTO.to_station='+stations[to]+'&purpose_codes=ADULT') as f:
         data = f.read()
         stations = read_station()
         print('Status:', f.status, f.reason)
-        info = json.loads(data.decode('utf-8'))
-        trains = aoa(info['data']['result'])
-        print('所有车次', len(trains))
-        good_trains = filter_train_by_time(trains)
-        print('符合条件的车次',list(map(lambda item : item[3], good_trains)))# 符合时间段的车次信息
-        for train in good_trains:
-            yw, yz, wz = train[sites['硬卧']], train[sites['硬座']], train[sites['无座']]
-            print(yw, yz, wz)
-            if(yw != '无' or yz != '无' ):
-                print('%s 有票了'%(train[3]))
-                print('\a')
-        # print('硬卧:%s,硬座:%s,无座:%s'%(k1102[sites["硬卧"]] , k1102[sites["硬座"]] , k1102[sites["无座"]]))
-        # if k1102[sites["硬卧"]] != '无':
-        #     print('\a\a\a\a')
+        try: 
+            info = json.loads(data.decode('utf-8'))
+            trains = aoa(info['data']['result'])
+            print('所有车次', len(trains))
+            good_trains = filter_train_by_time(trains)
+            print('符合条件的车次',list(map(lambda item : item[3], good_trains)))# 符合时间段的车次信息
+            for train in good_trains:
+                yw, yz, wz = train[sites['硬卧']], train[sites['硬座']], train[sites['无座']]
+                print(yw, yz, wz)
+                if(yw != '无' or yz != '无' ):
+                    print('%s 有票了'%(train[3]))
+                    print('\a')
+        except json.decoder.JSONDecodeError as e:
+            print('err')
+        counter += 1
+        print('====已查询%d次===='%(counter))
         time.sleep(3)
